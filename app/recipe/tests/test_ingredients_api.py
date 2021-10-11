1aa9dd7880e799b273f522789383e57b9eecd916
Raigato <gabriel.raymondou@gmail.com>
2021-10-11 22:31:15 +0200

    added ingredients api base

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Ingredient
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsAPITest(TestCase):
+    """Tests the ingredients API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Tests that login is required for retrieving ingredients"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITest(TestCase):
+    """Tests the ingredients API (private)"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('test@raigato.com', 'testpass')
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
